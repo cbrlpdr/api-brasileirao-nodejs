9ffcdef8c40b78a7a8a92460cae10d75f3a5e565
cbrlpdr <cbrl.pdr@gmail.com>
2025-09-01 19:58:14 -0400

    feat: enable CORS for all origins

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { json, Request, Response } from "express"
 import { getAllPlayers } from "./controllers/playerController";
 import { router } from "./routes/routes";
+import cors from "cors"
 
 export const initApp = () => {
     const app = express();
     app.use(json());
     app.use("/api", router);
+    app.use(cors())
     return app;
 }
